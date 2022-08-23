9964adb8c4f8513b43dd22360015c7c400135f3a
SakuraBurst <colapes2@gmail.com>
2022-08-24 02:27:36 +0300

    increment 6 fixes

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -10,7 +10,7 @@ import (
 type config struct {
 	ServerAddress   string `env:"SERVER_ADDRESS" envDefault:"localhost:8080"`
 	BaseURL         string `env:"BASE_URL" envDefault:"http://localhost:8080/"`
-	FileStoragePath string `env:"FILE_STORAGE_PATH" envDefault:"backUp.log"`
+	FileStoragePath string `env:"FILE_STORAGE_PATH"`
 }
 
 func main() {
